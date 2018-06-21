fe3dd10ae6226def5978748facef9cb4cd3aaa9b
Eric Lee <ericjoolee79@gmail.com>
2018-06-21 02:59:55 -0500

    Modified files + added one more picture for the list

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,164 +1,76 @@
 // Variables
-var teams = [
-    {
-        team: "argentina",
-        image: "assets/images/argentina.png"
-    },
-    {
-        team: "austrailia",
-        image: "assets/images/austrailia.png"
-    },
-    {
-        team: "belgium",
-        image: "assets/images/belgium.png"
-    },
-    {
-        team: "brazil",
-        image: "assets/images/brazil.png"
-    },
-    {
-        team: "colombia",
-        image: "assets/images/colombia.png"
-    },
-    {
-        team: "costa rica",
-        image: "assets/images/costa-rica.png"
-    },
-    {
-        team: "croatia",
-        image: "assets/images/belgium.png"
-    },
-    {
-        team: "denmark",
-        image: "assets/images/denmark.png"
-    },
-    {
-        team: "egypt",
-        image: "assets/images/egypt.png"
-    },
-    {
-        team: "england",
-        image: "assets/images/england.png"
-    },
-    {
-        team: "france",
-        image: "assets/images/france.png"
-    },
-    {
-        team: "germany",
-        image: "assets/images/germany.png"
-    },
-    {
-        team: "iceland",
-        image: "assets/images/iceland.png"
-    },
-    {
-        team: "iran",
-        image: "assets/images/iran.png"
-    },
-    {
-        team: "japan",
-        image: "assets/images/japan.png"
-    },
-    {
-        team: "mexico",
-        image: "assets/images/mexico.png"
-    },
-    {
-        team: "morocco",
-        image: "assets/images/morocco.png"
-    },
-    {
-        team: "nigeria",
-        image: "assets/images/nigeria.png"
-    },
-    {
-        team: "panama",
-        image: "assets/images/panama.png"
-    },
-    {
-        team: "peru",
-        image: "assets/images/peru.png"
-    },
-    {
-        team: "poland",
-        image: "assets/images/poland.png"
-    },
-    {
-        team: "portugal",
-        image: "assets/images/portugal.png"
-    },
-    {
-        team: "russia",
-        image: "assets/images/russia.png"
-    },
-    {
-        team: "saudi arabia",
-        image: "assets/images/saudi-arabia.png"
-    },
-    {
-        team: "senegal",
-        image: "assets/images/senegal.png"
-    },
-    {
-        team: "serbia",
-        image: "assets/images/serbia.png"
-    },
-    {
-        team: "south korea",
-        image: "assets/images/south-korea.png"
-    },
-    {
-        team: "spain",
-        image: "assets/images/spain.png"
-    },
-    {
-        team: "sweden",
-        image: "assets/images/sweden.png"
-    },
-    {
-        team: "switzerland",
-        image: "assets/images/switzerland.png"
-    },
-    {
-        team: "tunisia",
-        image: "assets/images/tunisia.png"
-    },
-    {
-        team: "uruguay",
-        image: "assets/images/uruguay.png"
-    }];
+var teams = ["argentina", "austrailia", "belgium", "brazil", "colombia", "costarica", "croatia", "denmark", "egypt", "england", "france", "germany", "iceland", "iran", "japan", "mexico", "morocco", "nigeria", "panama", "peru", "poland", "portugal", "russia", "saudiarabia", "senegal", "serbia", "southkorea", "spain", "sweden", "switzerland", "tunisia", "uruguay"];
+var chosenTeam = "";
+var lettersInChosenTeam = [];
+var numBlanks = 0;
+var blanksAndSuccesses = [];
+var wrongGuesses = [];
 
-var randomNum = Math.floor(Math.random() * teams.length);
-var choosenTeam = teams[randomNum];
-var underScore = [];
-var wrongWord = [];
+var winCounter = 0;
+var numGuesses = 10;
 
-var docUnderScore = document.getElementById("#underScore");
-var docWrongGuess = document.getElementById("#wrongGuess");
+function newGame() {
+    numGuesses = 10;
+    chosenTeam = teams[Math.floor(Math.random() * teams.length)];
+    lettersInChosenTeam = chosenTeam.split("");
+    numBlanks = lettersInChosenTeam.length;
 
-var genUndersocre = () => {
-    for(var i = 0; i < choosenTeam.length; i++) {
-        underScore.push("_");
+    blanksAndSuccesses = [];
+    wrongGuesses= [];
+
+    for (var i = 0; i < numBlanks; i++) {
+        blanksAndSuccesses.push("_");
     }
-    return underScore;
+    
+    document.getElementById("numGuessesRemaining").innerHTML = numGuesses;
+    document.getElementById("underScore").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("wrongGuess").innerHTML = wrongGuesses.join(" ");
 }
 
-document.addEventListener('keypress', (event) => {
-    var keyword = String.fromCharCode(keyCode);
-    if(choosenTeam.indexOf(keyword) > -1) {
-        rightWord.push(keyword);
-        
-        underScore[choosenTeam.indexOf(keyword)] = keyword;
-        docUnderScore[0].innerHTML = underScore.join('');
+function checkLetters(letter) {
+    var letterInTeam = false;
 
-        if(underScore.join('') == choosenTeam) {
-            alert("You Got It!");
+    for (var i = 0; i < numBlanks; i++) {
+        if (chosenTeam[i] === letter) {
+            letterInTeam = true;
+        }
+    }
+
+    if (letterInTeam) {
+        for (i = 0; i < numBlanks; i++) {
+            if (chosenTeam[i] === letter) {
+                blanksAndSuccesses[i] = letter;
+            }
         }
     } else {
-        wrongWord.push(keyword);
-        docWrongGuess[0].innerHTML = wrongWord;
+        wrongGuesses.push(letter);
+        numGuesses--;
     }
-});
+}
 
-UnderScore[0].innerHTML = genUndersocre().join('');
+function gameSet() {
+    document.getElementById("numGuessesRemaining").innerHTML = numGuesses;
+    document.getElementById("underScore").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("wrongGuess").innerHTML = wrongGuesses.join(" ");
+
+    if (lettersInChosenTeam.toString() === blanksAndSuccesses.toString()) {
+        winCounter++;
+        alert("GOAL GOAL GOAL!!!!!");
+
+        document.getElementById("numWins").innerHTML = winCounter;
+        newGame();
+    } 
+    
+    else if (numGuesses === 0) {
+        alert("Try Again!");
+        newGame();
+    }
+}
+
+newGame();
+
+document.onkeyup = function(event) {
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetters(letterGuessed);
+    gameSet();
+};
